f982564b3a228f721b9ecad543387850c5d78d22
Iwan Ruskiewicz <ivans.ruskevics@ideaportriga.lv>
2017-02-21 15:26:31 +0200

    Mapped data extraction method added to DAO

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 from service import dao
 
-data = dao.get_csv_data(delimiter=";")
+data = dao.get_mapped_data(delimiter=";")
 
-feature_names = data[0]
-values = data[1:]
-
-print("Records total:", len(values))
-print("Feature names:")
-print(feature_names)
+print("Records total:", len(data))
--- service/dao.py
+++ service/dao.py
@@ -6,3 +6,11 @@ DATA_PATH = "data/bank.csv"
 def get_csv_data(delimiter=',', quotechar='"'):
     stream = open(DATA_PATH, newline='')
     return list(csv.reader(stream, delimiter=delimiter, quotechar=quotechar, quoting=csv.QUOTE_NONNUMERIC))
+
+
+def get_mapped_data(delimiter=',', quotechar='"'):
+    rows = get_csv_data(delimiter=delimiter, quotechar=quotechar)
+    feature_names = rows[0]
+    raw_data = rows[1:]
+    mapped_data = [dict(zip(feature_names, item)) for item in raw_data]
+    return mapped_data
